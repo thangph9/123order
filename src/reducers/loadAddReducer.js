8e5206580c673c93141cabd7d7b1d615c76f0f6b
trjvjp97 <trjvjp97@gmail.com>
2018-09-07 09:11:29 +0700

    123

--- src/reducers/loadAddReducer.js
+++ src/reducers/loadAddReducer.js
@@ -1,10 +1,9 @@
-import React from "react";
+
 export function loadAdd(state=0,action){
 	switch (action.type) {
-		case 'LOAD_ADD':{
+		case 'LOAD_ADD':
 			
 			return action.numb;
-		};
 		default:
 			return state;
 	}	
