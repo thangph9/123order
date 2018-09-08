43f17ac25ad944346eaf8ae1c2af1ceb7d1585fc
trjvjp97 <trjvjp97@gmail.com>
2018-09-08 17:23:33 +0700

    page

--- server.js
+++ server.js
@@ -194,14 +194,14 @@ var ObjTable={
 });
 }*/
 function loadAmazonDealDay(){
-  models.instance.amazon_deal_day.eachRow({Name: 'John'}, {fetchSize : 100, pageState : pageState}, function(n, row){
+  models.instance.amazon_deal_day.eachRow({Name: 'John'}, {fetchSize : 100, pageState : 2}, function(n, row){
     // invoked per each row in all the pages
 }, function(err, result){
     // called once the page has been retrieved.
     if(err) throw err;
     // store the next paging state.
       pageState = result.pageState;
-      console.log(pageState);
+
 });
 }
 app.post("/home",function(req,res){
