db62827609407189aa779b78bc82adcecd5159da
trjvjp97 <trjvjp97@gmail.com>
2018-09-09 23:02:08 +0700

    123

--- server.js
+++ server.js
@@ -205,7 +205,7 @@ app.post("/home",function(req,res){
     })
 })
 app.post('/detail-product',function (req,res) {
-  console.log(req.body.dealid+' 1');
+  console.log(req.body.dealid);
   async.series([
       (callback)=>{
         models.instance.product_detail.find({dealid:'cbfe0f55'},function(err,result){
