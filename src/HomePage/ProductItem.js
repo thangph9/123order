b702d27ca65cde2c7e6b741a2333181dbf2351b7
trjvjp97 <trjvjp97@gmail.com>
2018-09-09 23:10:21 +0700

    13

--- src/HomePage/ProductItem.js
+++ src/HomePage/ProductItem.js
@@ -27,7 +27,7 @@ class ProductItem extends React.Component {
                         <div className="position-absolute discount-tag-pos"><img style={{width:'48px',height:'48px',position:'relative',right:'-10px'}} src="\img\sale3.png" alt='img' className="img-fluid" /></div>
                         <img style= {{width:'210px',height:'210px'}} className="card-img-top img-thumbnail border-0 img-fluid" src={data.img} alt='img'/>
                         <div className="overlay">
-                            <Link to={"/detail-product/"+data.dealid} onClick={()=>this.handleClickLink(this,data.dealid)} className="info">Chi tiết</Link>
+                            <Link to={"/detail-product/"+data.dealid} onClick={()=>this.handleClickLink(data.dealid)} className="info">Chi tiết</Link>
                         </div>
                     </div>
                     <div className="card-body p-2">
