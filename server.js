007e338fe9b8d3b9e5ca9e6d473996395d33f305
trjvjp97 <trjvjp97@gmail.com>
2018-09-09 14:25:37 +0700

    123

--- server.js
+++ server.js
@@ -208,7 +208,7 @@ app.post('/detail-product',function (req,res) {
   console.log(req.body.dealid);
   async.series([
       (callback)=>{
-        models.instance.product_detail.find({dealid:req.body.dealid},function(err,result){
+        models.instance.product_detail.findOne({dealid:req.body.dealid},function(err,result){
         var arr=result.map(item=>{
         return obj={
             dealid:item.dealid,
