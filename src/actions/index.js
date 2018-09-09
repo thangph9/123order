3a5d4e69c3c96868126a1fb56c67ef0d36a2a978
trjvjp97 <trjvjp97@gmail.com>
2018-09-09 17:10:31 +0700

    123

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,4 +2,4 @@ export * from "./alert.actions";
 export * from './loadImg.js';
 export * from './loadAdd.js';
 export * from './initLoad.js';
-export * from './mouseOverImage.js';
+ export * from './mouseOverImage.js';
