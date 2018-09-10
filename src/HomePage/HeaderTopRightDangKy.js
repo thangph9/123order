c1f6480a62d1f2d9002b617526583f473c939a0c
trjvjp97 <trjvjp97@gmail.com>
2018-09-11 00:30:02 +0700

    123

--- src/HomePage/HeaderTopRightDangKy.js
+++ src/HomePage/HeaderTopRightDangKy.js
@@ -1,6 +1,7 @@
 import React from "react";
 //import { Link } from "react-router-dom";
 import { connect } from "react-redux";
+import $ from 'jquery';
 class HeaderTopRightDangKy extends React.Component{
     constructor(props){
         super(props);
@@ -23,7 +24,11 @@ class HeaderTopRightDangKy extends React.Component{
 
         })
     }
-
+    componentDidMount(){
+        $("#signin").on('click', function(event) {
+            $("#myRegister").modal();
+        });
+    }
     render(){
         
         return (
