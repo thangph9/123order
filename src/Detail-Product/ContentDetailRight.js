2847f7fa765fa6418e48a87e5ffd13d6661865e0
trjvjp97 <trjvjp97@gmail.com>
2018-09-12 07:59:28 +0700

    123

--- src/Detail-Product/ContentDetailRight.js
+++ src/Detail-Product/ContentDetailRight.js
@@ -44,6 +44,7 @@ class ContentDetailRight extends React.Component {
     render() {
         var {loadDetail}=this.props;
         var {handleMouse}=this.props;
+        console.log(this.state.numbOut);
         return (
             <div onMouseOut={()=>this.handleOut()} className="col-md-1 xzoom-thumbs">
                 <span style={{cursor:'pointer'}} ><img onClick={()=>this.handleClickImage(1)} onMouseOver={()=>this.handleMouseOver(1)} alt="img" className={(this.state.numbOut===1) ? 'xzoom-gallery d-lg-block mb-2  xactive active-small-image': 'xzoom-gallery d-lg-block mb-2  xactive'} src={(loadDetail[0]!=undefined) ? loadDetail[0].smallimage[0]:'' } /></span>
