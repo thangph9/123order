e75c071a7c9a7fc959c0134bbb7b1551501c84b5
trjvjp97 <trjvjp97@gmail.com>
2018-09-13 21:35:25 +0700

    123

--- src/Detail-Product/ContentDetailRight.js
+++ src/Detail-Product/ContentDetailRight.js
@@ -28,23 +28,16 @@ class ContentDetailRight extends React.Component {
     handleMouseOver(value){
         this.props.dispatch(mouseOverSmallImageProduct(value));
     }
-    handleClickImage(value){
-        this.setState({
-            numbOut:value,
-        })
-        console.log(this.props.handleMouse);
-        this.props.dispatch(mouseClickSmallImageProduct(value));
-    }
     render() {
         var {loadDetail}=this.props;
         var {handleMouse}=this.props;
         return (
             <ul className="col-md-1 xzoom-thumbs">
-                <li style={{cursor:'pointer'}} ><img onClick={()=>this.handleClickImage(1)} onMouseOver={()=>this.handleMouseOver(1)} alt="img" className={(handleMouse==1) ? 'xzoom-gallery d-lg-block mb-2  xactive active-small-image': 'xzoom-gallery d-lg-block mb-2  xactive'} src={(loadDetail[0]!=undefined) ? loadDetail[0].smallimage[0]:'' } /></li>
-                <li style={{cursor:'pointer'}} ><img onClick={()=>this.handleClickImage(2)} onMouseOver={()=>this.handleMouseOver(2)} alt="img" className={(handleMouse==2) ? 'xzoom-gallery d-lg-block mb-2  xactive active-small-image': 'xzoom-gallery d-lg-block mb-2  xactive'} src={(loadDetail[0]!=undefined) ? loadDetail[0].smallimage[1] :''}/></li>
-                <li style={{cursor:'pointer'}} ><img onClick={()=>this.handleClickImage(3)} onMouseOver={()=>this.handleMouseOver(3)} alt="img" className={(handleMouse==3) ? 'xzoom-gallery d-lg-block mb-2  xactive active-small-image': 'xzoom-gallery d-lg-block mb-2  xactive'} src={(loadDetail[0]!=undefined) ? loadDetail[0].smallimage[2] : ''} /></li>
-                <li style={{cursor:'pointer'}} ><img onClick={()=>this.handleClickImage(4)} onMouseOver={()=>this.handleMouseOver(4)} alt="img" className={(handleMouse==4) ? 'xzoom-gallery d-lg-block mb-2  xactive active-small-image': 'xzoom-gallery d-lg-block mb-2  xactive'} src={(loadDetail[0]!=undefined) ? loadDetail[0].smallimage[3] : ''} /></li>
-                <li style={{cursor:'pointer'}} ><img onClick={()=>this.handleClickImage(5)} onMouseOver={()=>this.handleMouseOver(5)} alt="img" className={(handleMouse==5) ? 'xzoom-gallery d-lg-block mb-2  xactive active-small-image': 'xzoom-gallery d-lg-block mb-2  xactive'} src={(loadDetail[0]!=undefined) ? loadDetail[0].smallimage[4] :''} /></li>
+                <li style={{cursor:'pointer'}} ><img onMouseOver={()=>this.handleMouseOver(1)} alt="img" className={(handleMouse==1) ? 'xzoom-gallery d-lg-block mb-2  xactive active-small-image': 'xzoom-gallery d-lg-block mb-2  xactive'} src={(loadDetail[0]!=undefined) ? loadDetail[0].smallimage[0]:'' } /></li>
+                <li style={{cursor:'pointer'}} ><img onMouseOver={()=>this.handleMouseOver(2)} alt="img" className={(handleMouse==2) ? 'xzoom-gallery d-lg-block mb-2  xactive active-small-image': 'xzoom-gallery d-lg-block mb-2  xactive'} src={(loadDetail[0]!=undefined) ? loadDetail[0].smallimage[1] :''}/></li>
+                <li style={{cursor:'pointer'}} ><img onMouseOver={()=>this.handleMouseOver(3)} alt="img" className={(handleMouse==3) ? 'xzoom-gallery d-lg-block mb-2  xactive active-small-image': 'xzoom-gallery d-lg-block mb-2  xactive'} src={(loadDetail[0]!=undefined) ? loadDetail[0].smallimage[2] : ''} /></li>
+                <li style={{cursor:'pointer'}} ><img onMouseOver={()=>this.handleMouseOver(4)} alt="img" className={(handleMouse==4) ? 'xzoom-gallery d-lg-block mb-2  xactive active-small-image': 'xzoom-gallery d-lg-block mb-2  xactive'} src={(loadDetail[0]!=undefined) ? loadDetail[0].smallimage[3] : ''} /></li>
+                <li style={{cursor:'pointer'}} ><img onMouseOver={()=>this.handleMouseOver(5)} alt="img" className={(handleMouse==5) ? 'xzoom-gallery d-lg-block mb-2  xactive active-small-image': 'xzoom-gallery d-lg-block mb-2  xactive'} src={(loadDetail[0]!=undefined) ? loadDetail[0].smallimage[4] :''} /></li>
             </ul>
         )
 
