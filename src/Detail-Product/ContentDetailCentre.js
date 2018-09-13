2dfaf3500f8ca13b1a1635098ae8c828f3143d24
trjvjp97 <trjvjp97@gmail.com>
2018-09-13 18:19:00 +0700

    123

--- src/Detail-Product/ContentDetailCentre.js
+++ src/Detail-Product/ContentDetailCentre.js
@@ -35,14 +35,13 @@ class ContentDetailCentre extends React.Component {
 							alt: 'img',
 							width:350,
 							height:350,
-							src: (loadDetail[0]!=undefined ) ? loadDetail[0].largeimage[mouseOverImage]:''
+							src: '/img/femmecalm.png'
 						},
 						largeImage: {
-							src: (loadDetail[0]!=undefined ) ? loadDetail[0].largeimage[mouseOverImage]:'',
+							src: '/img/femmecalm.png',
 							width: 425,
 							height: 425
-						}
-						
+						}		
 			}} />
             </div>
 
