6e9ff77038fd4c695057a1915a165b2f21f5d3d5
trjvjp97 <trjvjp97@gmail.com>
2018-09-18 18:14:27 +0700

    123

--- src/Landing-Page/ProductItem.js
+++ src/Landing-Page/ProductItem.js
@@ -9,17 +9,8 @@ class ProductItem extends React.Component {
 
         }
     }
-    handleClickLink(value){
-        this.props.dispatch(mouseClickLinkProductItem(value));
-    }
+   
     render(){
-
-        var display=this.props.customStyle;
-        var {data}=this.props;
-        if (data.title.length>65){
-            data.title= data.title.slice(0,65)+'...';
-        }
-        console.log(data);
         return (
             <div className="col-md mt-2 px-2" style={{display:display,marginBottom:'8px'}}>
                 <div className="card" style={{width:'212px',height:'340px'}}>
@@ -27,7 +18,7 @@ class ProductItem extends React.Component {
                     <span style={{position: 'absolute',top: '10px',right: '10px',background: 'red',color: '#fff',fontSize: '13px',fontWeight: 700,borderRadius: '3px',padding: '2px 5px',zIndex: 9}}>-64%</span>
                         <img style= {{width:'210px',height:'210px'}} className="card-img-top img-thumbnail border-0 img-fluid" src="https://static-v3.weshop.com.vn/upload/7/0/z/m/a/e/z/4/4/9/michael-kors-mk6113-women-s--22713.jpg" alt='img'/>
                         <div className="overlay">
-                            <Link to={"/detail-product/"+data.dealid} onClick={()=>this.handleClickLink(data)} className="info">Chi tiết</Link>
+                            <Link to={"/detail-product/"+data.dealid} className="info">Chi tiết</Link>
                         </div>
                     </div>
                     <div className="card-body p-2">
