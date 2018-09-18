65eeac65c3acb065b1eb665274ca9fde4673fa51
trjvjp97 <trjvjp97@gmail.com>
2018-09-18 17:15:31 +0700

    123

---
+++ src/Landing-Page/Content.js
@@ -0,0 +1,35 @@
+import React from "react";
+import { connect } from "react-redux";
+import { ContentSale } from "./ContentSlide";
+
+class Content extends React.Component{
+    constructor(props){
+        super(props);
+        this.state={
+            login: true,
+            loading: false,
+        }
+        this.handleSubmit=this.handleSubmit.bind(this);
+    }
+    handleSubmit(){
+        this.setState({
+            login: false
+        })
+        console.log("Hello World")
+    }
+    render(){
+        
+        return (
+            <div>
+                <ContentSlide/>
+            </div>
+        )
+        
+    }
+}
+function mapStateToProps(state){
+  
+    return  state;
+}
+const connectedContent=connect(mapStateToProps)(Content);
+export { connectedContent as Content } 
---
+++ src/Landing-Page/ContentSlide.js
@@ -0,0 +1,47 @@
+import React from "react";
+import { connect } from "react-redux";
+import Slider from "react-slick";
+class ContentSlide extends React.Component{
+    constructor(props){
+        super(props);
+        this.state={
+            login: true,
+            loading: false,
+        }
+        this.handleSubmit=this.handleSubmit.bind(this);
+    }
+    handleSubmit(){
+        this.setState({
+            login: false
+        })
+        console.log("Hello World")
+    }
+    render(){
+        var settings = {
+            dots: true,
+            infinite: true,
+            speed: 500,
+            slidesToShow: 1,
+            slidesToScroll: 1
+        };
+        return (
+            <div className="container mt-2">
+                <div className="row">
+                <Slider {...settings}>
+                    <img alt='landing-img' src="https://static-v3.weshop.com.vn/upload/cms/18/09/08/k/d/v/7/v/1.jpg" style={{width:'1140px'}} />
+                    <img alt='landing-img' src="https://static-v3.weshop.com.vn/upload/cms/18/09/08/m/c/f/e/o/1349x500.jpg" style={{width:'1140px'}} />
+                    <img alt='landing-img' src="https://static-v3.weshop.com.vn/upload/cms/18/09/08/k/d/v/7/v/1.jpg" style={{width:'1140px'}} />
+                    <img alt='landing-img' src="https://static-v3.weshop.com.vn/upload/cms/18/09/08/m/c/f/e/o/1349x500.jpg" style={{width:'1140px'}} />
+                </Slider>
+                </div>
+            </div>
+        )
+        
+    }
+}
+function mapStateToProps(state){
+  
+    return state;
+}
+const connected=connect(mapStateToProps)(ContentSlide);
+export { connected as ContentSlide } 
