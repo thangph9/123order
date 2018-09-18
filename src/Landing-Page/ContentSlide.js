22916a1e1b99f57fd8fdd3b7555b0ee1c9e3857e
trjvjp97 <trjvjp97@gmail.com>
2018-09-18 17:24:39 +0700

    123

--- src/Landing-Page/ContentSlide.js
+++ src/Landing-Page/ContentSlide.js
@@ -18,24 +18,25 @@ class ContentSlide extends React.Component{
     }
     render(){
         var settings = {
-            dots: true,
+            dots: false,
             infinite: true,
             speed: 500,
             slidesToShow: 1,
-            slidesToScroll: 1
+            slidesToScroll: 1,
+            autoplay: true,
+            autoplaySpeed: 3000,
+            arrows: false
         };
         return (
             <div className="container mt-2">
-                <div className="row">
-                    <div className="col-12">
+                
                         <Slider {...settings}>
                             <img alt='landing-img' src="https://static-v3.weshop.com.vn/upload/cms/18/09/08/k/d/v/7/v/1.jpg" style={{width:'1140px'}} />
                             <img alt='landing-img' src="https://static-v3.weshop.com.vn/upload/cms/18/09/08/m/c/f/e/o/1349x500.jpg" style={{width:'1140px'}} />
                             <img alt='landing-img' src="https://static-v3.weshop.com.vn/upload/cms/18/09/08/k/d/v/7/v/1.jpg" style={{width:'1140px'}} />
                             <img alt='landing-img' src="https://static-v3.weshop.com.vn/upload/cms/18/09/08/m/c/f/e/o/1349x500.jpg" style={{width:'1140px'}} />
                         </Slider>
-                    </div>
-                </div>
+               
             </div>
         )
         
