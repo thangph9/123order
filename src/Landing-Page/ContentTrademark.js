162db4b3151841ff1922477b7d11fb4e6f2df81a
trjvjp97 <trjvjp97@gmail.com>
2018-09-18 18:24:05 +0700

    123

--- src/Landing-Page/ContentTrademark.js
+++ src/Landing-Page/ContentTrademark.js
@@ -22,22 +22,22 @@ class ContentTrademark extends React.Component{
             <div className="main-tabs">
             <div className="container">
                 <ul className="list-inline">
-                    <li className=""><a href="https://weshop.com.vn/search/bulova%20women%20watch.html?portal=ebay">Bulova</a></li>
-                    <li className=""><a href="https://weshop.com.vn/search/citizen%20women%20watch.html?portal=ebay">Citizen</a></li>
-                    <li className=""><a href="https://weshop.com.vn/search/seiko%20women%20watch.html?portal=ebay">Seiko</a></li>
-                    <li className=""><a href="https://weshop.com.vn/search/burgi%20women%20watch.html?portal=ebay">Burgi</a></li>
-                    <li className=""><a href="https://weshop.com.vn/search/akribos%20women%20watch.html?portal=ebay">Akribos</a></li>
-                    <li className=""><a href="https://weshop.com.vn/search/skagen%20women%20watch.html?portal=ebay">Skagen</a></li>
-                    <li className=""><a href="https://weshop.com.vn/search/anne%20klein%20women%20watch.html?portal=ebay">Anne Klein</a></li>
-                    <li className=""><a href="https://weshop.com.vn/search/guess%20women%20watch.html?portal=ebay">Guess</a></li>
-                    <li className=""><a href="https://weshop.com.vn/search/casio%20women%20watch.html?portal=ebay">Casio</a></li>
-                    <li className=""><a href="https://weshop.com.vn/search/invicta%20women%20watch.html?portal=ebay">Invicta</a></li>
-                    <li className=""><a href="https://weshop.com.vn/search/michael%20kors%20women%20watch.html?portal=ebay">Michael Kors</a></li>
-                    <li className=""><a href="https://weshop.com.vn/search/%20Daniel%20Wellington%20women%20watch.html?portal=ebay">Daniel Wellington</a></li>
-                    <li className=""><a href="https://weshop.com.vn/search/fossil%20women%20watch.html?portal=ebay">Fossil</a></li>
-                    <li className=""><a href="https://weshop.com.vn/search/Kenneth%20Cole%20women%20watch.html?portal=ebay">Kenneth Cole</a></li>
-                    <li className=""><a href="https://weshop.com.vn/search/Timex%20women%20watch.html?portal=ebay">Timex</a></li>
-                    <li className=""><a href="https://weshop.com.vn/search/Kate%20Spade%20women%20watch.html?portal=ebay">Kate Spade</a></li>
+                    <li className=""><a style={{fontSize: '14px',fontWeight: 400,textDecoration:'none'}} href="https://weshop.com.vn/search/bulova%20women%20watch.html?portal=ebay">Bulova</a></li>
+                    <li className=""><a style={{fontSize: '14px',fontWeight: 400,textDecoration:'none'}} href="https://weshop.com.vn/search/citizen%20women%20watch.html?portal=ebay">Citizen</a></li>
+                    <li className=""><a style={{fontSize: '14px',fontWeight: 400,textDecoration:'none'}} href="https://weshop.com.vn/search/seiko%20women%20watch.html?portal=ebay">Seiko</a></li>
+                    <li className=""><a style={{fontSize: '14px',fontWeight: 400,textDecoration:'none'}} href="https://weshop.com.vn/search/burgi%20women%20watch.html?portal=ebay">Burgi</a></li>
+                    <li className=""><a style={{fontSize: '14px',fontWeight: 400,textDecoration:'none'}} href="https://weshop.com.vn/search/akribos%20women%20watch.html?portal=ebay">Akribos</a></li>
+                    <li className=""><a style={{fontSize: '14px',fontWeight: 400,textDecoration:'none'}} href="https://weshop.com.vn/search/skagen%20women%20watch.html?portal=ebay">Skagen</a></li>
+                    <li className=""><a style={{fontSize: '14px',fontWeight: 400,textDecoration:'none'}} href="https://weshop.com.vn/search/anne%20klein%20women%20watch.html?portal=ebay">Anne Klein</a></li>
+                    <li className=""><a style={{fontSize: '14px',fontWeight: 400,textDecoration:'none'}} href="https://weshop.com.vn/search/guess%20women%20watch.html?portal=ebay">Guess</a></li>
+                    <li className=""><a style={{fontSize: '14px',fontWeight: 400,textDecoration:'none'}} href="https://weshop.com.vn/search/casio%20women%20watch.html?portal=ebay">Casio</a></li>
+                    <li className=""><a style={{fontSize: '14px',fontWeight: 400,textDecoration:'none'}} href="https://weshop.com.vn/search/invicta%20women%20watch.html?portal=ebay">Invicta</a></li>
+                    <li className=""><a style={{fontSize: '14px',fontWeight: 400,textDecoration:'none'}} href="https://weshop.com.vn/search/michael%20kors%20women%20watch.html?portal=ebay">Michael Kors</a></li>
+                    <li className=""><a style={{fontSize: '14px',fontWeight: 400,textDecoration:'none'}} href="https://weshop.com.vn/search/%20Daniel%20Wellington%20women%20watch.html?portal=ebay">Daniel Wellington</a></li>
+                    <li className=""><a style={{fontSize: '14px',fontWeight: 400,textDecoration:'none'}} href="https://weshop.com.vn/search/fossil%20women%20watch.html?portal=ebay">Fossil</a></li>
+                    <li className=""><a style={{fontSize: '14px',fontWeight: 400,textDecoration:'none'}} href="https://weshop.com.vn/search/Kenneth%20Cole%20women%20watch.html?portal=ebay">Kenneth Cole</a></li>
+                    <li className=""><a style={{fontSize: '14px',fontWeight: 400,textDecoration:'none'}} href="https://weshop.com.vn/search/Timex%20women%20watch.html?portal=ebay">Timex</a></li>
+                    <li className=""><a style={{fontSize: '14px',fontWeight: 400,textDecoration:'none'}} href="https://weshop.com.vn/search/Kate%20Spade%20women%20watch.html?portal=ebay">Kate Spade</a></li>
                 </ul>
             </div>
             </div>
