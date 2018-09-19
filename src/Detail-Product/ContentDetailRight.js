089a9c47bb9a73c909e3d525c581f741795185b7
trjvjp97 <trjvjp97@gmail.com>
2018-09-19 22:34:46 +0700

    123

--- src/Detail-Product/ContentDetailRight.js
+++ src/Detail-Product/ContentDetailRight.js
@@ -32,9 +32,9 @@ class ContentDetailRight extends React.Component {
         let menuItems = [];
         var {loadDetail}=this.props;
         var {handleMouse}=this.props;
-        console.log(loadDetail[0].smallimage[i]);
+        console.log(loadDetail[0].smallimage[0]);
         for(var i=0;i<loadDetail[0].smallimage.length;i++){
-            menuItems.push(<li key={i} style={{cursor:'pointer'}} ><img onMouseOver={()=>this.handleMouseOver(i)} alt="img" className={(handleMouse==i) ? 'xzoom-gallery d-lg-block mb-2  xactive active-small-image': 'xzoom-gallery d-lg-block mb-2  xactive'} src={(loadDetail[0]!=undefined) ? loadDetail[0].smallimage[i]:'' } /></li>);
+            menuItems.push(<li key={i} style={{cursor:'pointer'}} ><img onMouseOver={()=>this.handleMouseOver(i)} alt="img" className={(handleMouse==i) ? 'xzoom-gallery d-lg-block mb-2  xactive active-small-image': 'xzoom-gallery d-lg-block mb-2  xactive'} src={(loadDetail[0]!=undefined) ? loadDetail[0].smallimage[1]:'' } /></li>);
         }
         return (
             <ul className="col-md-1 xzoom-thumbs">
