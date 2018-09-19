5d9464599af72ec705fd12f39e3d09a0c836ae6c
trjvjp97 <trjvjp97@gmail.com>
2018-09-19 22:10:39 +0700

    123

--- src/Detail-Product/ContentDetailRight.js
+++ src/Detail-Product/ContentDetailRight.js
@@ -29,15 +29,15 @@ class ContentDetailRight extends React.Component {
         this.props.dispatch(mouseOverSmallImageProduct(value));
     }
     render() {
+        let menuItems = [];
         var {loadDetail}=this.props;
         var {handleMouse}=this.props;
+        for(var i=0;i<loadDetail[0].smallimage.length;i++){
+            menuItems.push(<li key={i} style={{cursor:'pointer'}} ><img onMouseOver={()=>this.handleMouseOver(i)} alt="img" className={(handleMouse==i) ? 'xzoom-gallery d-lg-block mb-2  xactive active-small-image': 'xzoom-gallery d-lg-block mb-2  xactive'} src={(loadDetail[0]!=undefined) ? loadDetail[0].smallimage[i]:'' } /></li>);
+        }
         return (
             <ul className="col-md-1 xzoom-thumbs">
-                <li style={{cursor:'pointer'}} ><img onMouseOver={()=>this.handleMouseOver(1)} alt="img" className={(handleMouse==1) ? 'xzoom-gallery d-lg-block mb-2  xactive active-small-image': 'xzoom-gallery d-lg-block mb-2  xactive'} src={(loadDetail[0]!=undefined) ? loadDetail[0].smallimage[0]:'' } /></li>
-                <li style={{cursor:'pointer'}} ><img onMouseOver={()=>this.handleMouseOver(2)} alt="img" className={(handleMouse==2) ? 'xzoom-gallery d-lg-block mb-2  xactive active-small-image': 'xzoom-gallery d-lg-block mb-2  xactive'} src={(loadDetail[0]!=undefined) ? loadDetail[0].smallimage[1] :''}/></li>
-                <li style={{cursor:'pointer'}} ><img onMouseOver={()=>this.handleMouseOver(3)} alt="img" className={(handleMouse==3) ? 'xzoom-gallery d-lg-block mb-2  xactive active-small-image': 'xzoom-gallery d-lg-block mb-2  xactive'} src={(loadDetail[0]!=undefined) ? loadDetail[0].smallimage[2] : ''} /></li>
-                <li style={{cursor:'pointer'}} ><img onMouseOver={()=>this.handleMouseOver(4)} alt="img" className={(handleMouse==4) ? 'xzoom-gallery d-lg-block mb-2  xactive active-small-image': 'xzoom-gallery d-lg-block mb-2  xactive'} src={(loadDetail[0]!=undefined) ? loadDetail[0].smallimage[3] : ''} /></li>
-                <li style={{cursor:'pointer'}} ><img onMouseOver={()=>this.handleMouseOver(5)} alt="img" className={(handleMouse==5) ? 'xzoom-gallery d-lg-block mb-2  xactive active-small-image': 'xzoom-gallery d-lg-block mb-2  xactive'} src={(loadDetail[0]!=undefined) ? loadDetail[0].smallimage[4] :''} /></li>
+                {menuItems}
             </ul>
         )
 
