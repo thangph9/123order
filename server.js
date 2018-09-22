c0c6a237e5d8f2306d9e4aa217cf9683d470ed0c
trjvjp97 <trjvjp97@gmail.com>
2018-09-22 10:33:53 +0700

    123

--- server.js
+++ server.js
@@ -190,6 +190,7 @@ app.post("/home",jsonParser, function (req, res) {
 })
 var PARAM_IS_PRODUCT_DETAIL={};
 app.post('/detail-product',jsonParser, function (req, res) {
+  console.log(req.body.dealid);
   params=req.body;
   async.series([
     (callback)=>{
