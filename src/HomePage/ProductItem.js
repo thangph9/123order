4d7be0b1d9dbd328f72318e5d550729f1598ec68
trjvjp97 <trjvjp97@gmail.com>
2018-09-25 05:19:36 +0700

    123

--- src/HomePage/ProductItem.js
+++ src/HomePage/ProductItem.js
@@ -19,7 +19,7 @@ class ProductItem extends React.Component {
         if (data.title.length>65){
             data.title= data.title.slice(0,65)+'...';
         }
-        titleTrim.replace(/ /g , "-");
+        titleTrim=titleTrim.replace(/ /g , "-");
         return (
             <div className="col-md mt-2 px-2" style={{display:display,marginBottom:'8px'}}>
                 <div className="card" style={{width:'212px',height:'340px'}}>
