35734deb5203bb3519140345db2fa69784d75d87
trjvjp97 <trjvjp97@gmail.com>
2018-09-25 03:12:03 +0700

    123

--- server.js
+++ server.js
@@ -173,7 +173,7 @@ app.post("/home",jsonParser, function (req, res) {
             review: item.review,
             reviewlink: item.reviewlink,
             robot_label_track: item.robot_label_track,
-            sale: item.sale,
+            sale: item.sale.slice(1,4),
             stt: item.stt,
             timestamp: item.timestamp + "",
             title: item.title
