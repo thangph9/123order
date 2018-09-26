0619e7e164f5e10683aa54fc89cd2083839dc7ee
trjvjp97 <trjvjp97@gmail.com>
2018-09-27 04:28:28 +0700

    123

--- src/HomePage/ProductItem.js
+++ src/HomePage/ProductItem.js
@@ -27,7 +27,7 @@ class ProductItem extends React.Component {
                     <span className={(data.sale=='')?'none-hide':''} style={{position: 'absolute',top: '10px',right: '10px',background: 'red',color: '#fff',fontSize: '13px',fontWeight: 700,borderRadius: '3px',padding: '2px 5px',zIndex: 9}}>{data.sale}</span>
                         <img style= {{width:'210px',height:'210px'}} className="card-img-top img-thumbnail border-0 img-fluid" src={data.img} alt='img'/>
                         <div className="overlay">
-                            <Link to={`/detail-product/${titleTrim.substring(0,titleTrim.length-3)}.`+data.dealid} onClick={()=>this.handleClickLink(data)} className="info">Chi tiết</Link>
+                            <Link to={`/detail-product/${titleTrim.substring(0,titleTrim.length-3)}.` +data.dealid} onClick={()=>this.handleClickLink(data)} className="info">Chi tiết</Link>
                         </div>
                     </div>
                     <div className="card-body p-2">
