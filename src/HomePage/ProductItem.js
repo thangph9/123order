80e7bdd31f7d6adc70d3ed26d8c178f1eb02a805
trjvjp97 <trjvjp97@gmail.com>
2018-09-27 09:05:04 +0700

    123

--- src/HomePage/ProductItem.js
+++ src/HomePage/ProductItem.js
@@ -34,20 +34,20 @@ class ProductItem extends React.Component {
                     <div className="card-body p-2">
                         <h5 className="card-title deal-title-font" style={{ height:'32px' }}>{data.title}</h5>
                         {/* Starchart */}
-                        <div class="star-rating">
-							<div class="back-stars">
-								<i class="fa fa-star" aria-hidden="true"></i>
-								<i class="fa fa-star" aria-hidden="true"></i>
-								<i class="fa fa-star" aria-hidden="true"></i>
-								<i class="fa fa-star" aria-hidden="true"></i>
-								<i class="fa fa-star" aria-hidden="true"></i>
+                        <div className="star-rating">
+							<div className="back-stars">
+								<i className="fa fa-star" aria-hidden="true"></i>
+								<i className="fa fa-star" aria-hidden="true"></i>
+								<i className="fa fa-star" aria-hidden="true"></i>
+								<i className="fa fa-star" aria-hidden="true"></i>
+								<i className="fa fa-star" aria-hidden="true"></i>
 								
-								<div class="front-stars" style="width: 89.2%">
-									<i class="fa fa-star" aria-hidden="true"></i>
-									<i class="fa fa-star" aria-hidden="true"></i>
-									<i class="fa fa-star" aria-hidden="true"></i>
-									<i class="fa fa-star" aria-hidden="true"></i>
-									<i class="fa fa-star" aria-hidden="true"></i>
+								<div className="front-stars" style={{width:'84.6%'}}>
+									<i className="fa fa-star" aria-hidden="true"></i>
+									<i className="fa fa-star" aria-hidden="true"></i>
+									<i className="fa fa-star" aria-hidden="true"></i>
+									<i className="fa fa-star" aria-hidden="true"></i>
+									<i className="fa fa-star" aria-hidden="true"></i>
 								</div>
 							</div>
 						</div>  
