b0eaaabd008b525e70580630c1abdabb65502f94
trjvjp97 <trjvjp97@gmail.com>
2018-10-09 21:08:08 +0700

    123

--- src/HomePage/MenuHideA/MenuHideLeft.js
+++ src/HomePage/MenuHideA/MenuHideLeft.js
@@ -23,24 +23,26 @@ class MenuHideLeft extends React.Component {
     }
     render() {
         var { LoadCategory } = this.props;
-        var categoryItems = [];
         if (LoadCategory.length > 0) {
-            for (var i = 9; i < 18; i++) {
-                categoryItems.push(
-                    <div key={i} className="row tablinks pl-4 my-3" onMouseOver={() => this.handleMouseOver(LoadCategory[i])}>
-                        <div className="col-10">
-                            <span>{LoadCategory[i].category}</span>
-                        </div>
-                    </div>)
-            }
+            LoadCategory.splice(0,9);
+            LoadCategory.splice(9,9);
+            
         }
         return (
             <div className="container col-md-3 tab">
-                {categoryItems}
+                {(LoadCategory.length > 0)&&LoadCategory.map((value,index)=>{
+                    return(
+                        <div key={i} className="row tablinks pl-4 my-3" onMouseOver={() => this.handleMouseOver(value)}>
+                        <div className="col-10">
+                            <span>{value.category}</span>
+                        </div>
+                    </div>
+                    )                  
+                })}
                 <div className="row pl-4">
                     <div className="nav-line col-12" />
                 </div>
-                <div className="row tablinks pl-4 my-3"    >
+                <div className="row     tablinks pl-4 my-3"    >
                     <div className="col-8">
                         <span>Xem toàn bộ danh mục</span>
                     </div>
