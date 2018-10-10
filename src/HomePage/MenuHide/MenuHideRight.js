df6326eb932b8f9cce7f5c375c1017fb28c49518
trjvjp97 <trjvjp97@gmail.com>
2018-10-10 11:48:17 +0700

    123

--- src/HomePage/MenuHide/MenuHideRight.js
+++ src/HomePage/MenuHide/MenuHideRight.js
@@ -2,72 +2,75 @@ import React from "react";
 import { connect } from "react-redux";
 import { Link } from "react-router-dom";
 class MenuHideRight extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            login: true,
-            loading: false,
-        }
-        this.handleSubmit = this.handleSubmit.bind(this);
+  constructor(props) {
+    super(props);
+    this.state = {
+      login: true,
+      loading: false,
     }
-    handleSubmit() {
-        this.setState({
-            login: false
-        })
-        console.log("Hello World")
-    }
-    render() {
-        var {mouseOver}=this.props;
-        var {LoadCategoryIndexSencond}=this.props;
-        return (
-          <div className="col-md-9">
-                        <div id="congNghiep" className={(mouseOver==undefined)?'tabcontent':'tabcontent block-show'} style={{ display: 'none' }}>
-                <div className="container" aria-haspopup="true" aria-expanded="false">
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+  handleSubmit() {
+    this.setState({
+      login: false
+    })
+    console.log("Hello World")
+  }
+  render() {
+    var { mouseOver } = this.props;
+    var { LoadCategoryIndexSencond } = this.props;
+    console.log(LoadCategoryIndexSencond);
+    return (
+      <div className="col-md-9">
+        <div id="congNghiep" className={(mouseOver == undefined) ? 'tabcontent' : 'tabcontent block-show'} style={{ display: 'none' }}>
+          <div className="container" aria-haspopup="true" aria-expanded="false">
+            <div className="row">
+              <div className="col-md-8">
                 <div className="row">
-  <div className="col-md-8">
-    <div className="row">
-      <div className="col-md-12 gia-soc-title">
-        <span className="title-stroke ml-2" />
-        <span className="ml-2">{mouseOver.category}</span>
-      </div>
-    </div>
-    <div className="row mt-2">
-      <div className="col-md-4">
-        <ul>
-          <span className="list-tittle">{LoadCategoryIndexSencond.category}</span>
-          <li>
-            <Link to="/danh-muc-ebay" className="ml-2 text-dark  list-item">Đồ chơi lego</Link>
-          </li>
-          <li>    
-            <Link to="/danh-muc-ebay" className="ml-2 text-dark list-item">Đồ chơi barbie</Link>
-          </li>
-          <li>    
-            <Link to="/danh-muc-ebay" className="ml-2 text-dark list-item">Đồ chơi giáo dục</Link>
-          </li>
-          <li>    
-            <Link to="/danh-muc-ebay" className="ml-2 text-dark list-item">Đồ chơi thông minh</Link>
-          </li>
-        </ul>
-      </div>
-    </div>
-  </div>
-  <div className="col-md-4">
-    <img src="/img/dropdown-comerical.png" alt='img' className="img-fluid pr-2" />
-  </div>
-</div>
+                  <div className="col-md-12 gia-soc-title">
+                    <span className="title-stroke ml-2" />
+                    <span className="ml-2">{mouseOver.category}</span>
+                  </div>
                 </div>
+                <div className="row mt-2">
+                  {LoadCategoryIndexSencond.length > 0 && LoadCategoryIndexSencond.map((value, index) => {
+                    <div key={index} className="col-md-4">
+                      <ul>
+                        <span className="list-tittle">{value.category}</span>
+                        <li>
+                          <Link to="/danh-muc-ebay" className="ml-2 text-dark  list-item">Đồ chơi lego</Link>
+                        </li>
+                        <li>
+                          <Link to="/danh-muc-ebay" className="ml-2 text-dark list-item">Đồ chơi barbie</Link>
+                        </li>
+                        <li>
+                          <Link to="/danh-muc-ebay" className="ml-2 text-dark list-item">Đồ chơi giáo dục</Link>
+                        </li>
+                        <li>
+                          <Link to="/danh-muc-ebay" className="ml-2 text-dark list-item">Đồ chơi thông minh</Link>
+                        </li>
+                      </ul>
+                    </div>
+                  })}
+                </div>
+              </div>
+              <div className="col-md-4">
+                <img src="/img/dropdown-comerical.png" alt='img' className="img-fluid pr-2" />
+              </div>
             </div>
           </div>
-        )
+        </div>
+      </div>
+    )
 
-    }
+  }
 }
 function mapStateToProps(state) {
 
-    return {
-      mouseOver:state.mouseOverCategory,
-      LoadCategoryIndexSencond:state.initLoadCategoryIndexSecond
-    }
+  return {
+    mouseOver: state.mouseOverCategory,
+    LoadCategoryIndexSencond: state.initLoadCategoryIndexSecond
+  }
 }
 const connected = connect(mapStateToProps)(MenuHideRight);
 export { connected as MenuHideRight } 
