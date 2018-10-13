1657058f7c489002994a096f42f73138f5bba2f7
trjvjp97 <trjvjp97@gmail.com>
2018-10-13 10:20:34 +0700

    123

--- src/Category/CategoryMainRightHead.js
+++ src/Category/CategoryMainRightHead.js
@@ -15,7 +15,7 @@ class CategoryMainRightHead extends React.Component {
         console.log(childCate);
         if(childCate.length>0){
             childCate.forEach((value,index)=>{
-                summaryCategory(value.groupid,value.categoryindex+1)
+                this.summaryCategory(value.groupid,value.categoryindex+1)
             })
         }
     }
