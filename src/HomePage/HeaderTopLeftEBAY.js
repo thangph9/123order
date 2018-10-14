afeeb256831f00b7ce3f661f5741ed54617fdefb
trjvjp97 <trjvjp97@gmail.com>
2018-10-14 17:08:23 +0700

    123

--- src/HomePage/HeaderTopLeftEBAY.js
+++ src/HomePage/HeaderTopLeftEBAY.js
@@ -57,9 +57,9 @@ class HeaderTopLeftEBAY extends React.Component {
         var { LoadCate } = this.props;
         return (
             <li className="nav-item dropdown" id='menuEbay' >
-                <div className='nav-link dropdown-toggle dropdown-arrow text-white' onMouseOver={(LoadCate.length > 0) ? () => this.handleMouseOver(LoadCate[0]) : () => this.handleMouseOver(0)}>
+                <Link to="/danh-muc-ebay" className='nav-link text-white' onMouseOver={(LoadCate.length > 0) ? () => this.handleMouseOver(LoadCate[0]) : () => this.handleMouseOver(0)}>
                     EBAY
-                </div>
+                </Link>
                 <div className="container dropdown-menu ebay-position menu2-dropdown bg-white" aria-haspopup="true" aria-expanded="false" id='linkEbay'>
                     <MenuHide />
                 </div>
