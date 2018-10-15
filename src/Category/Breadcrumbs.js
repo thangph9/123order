c09760ae6b0598f1e7a83d1a439047f0dced665b
trjvjp97 <trjvjp97@gmail.com>
2018-10-15 11:40:23 +0700

    123

--- src/Category/Breadcrumbs.js
+++ src/Category/Breadcrumbs.js
@@ -1,27 +1,14 @@
 import React from "react";
 import { connect } from "react-redux";
-import { Link } from "react-router-dom";
 class Breadcrumbs extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
             login: true,
             loading: false,
-            arrBreadcrumb: []
         }
     }
     render() {
-        var { mouseClickCategory } = this.props;
-        var { initLoadCategoryItem } = this.props;
-        var parentCate = initLoadCategoryItem.filter((item) => {
-            return item.nodeid == mouseClickCategory.nodeid;
-        })
-        if (this.state.arrBreadcrumb.length <= mouseClickCategory.categoryindex - 1) {
-            this.setState({
-                arrBreadcrumb: this.state.arrBreadcrumb.concat(parentCate)
-            })
-        }
-
         return (
             <section id="breadcrumbs-block-v2" className="breadcrumbs-block-v2" style={{ marginBottom: 30 }}>
                 <div className="container-fedo" itemScope="itemscope" itemType="http://schema.org/BreadcrumbList" style={{ padding: 0, height: 33 }}>
@@ -30,21 +17,13 @@ class Breadcrumbs extends React.Component {
                         <li className="break" />
                         <li><a href="/us/amazon-store/" itemProp="item"><span itemProp="name">Amazon Mỹ</span></a></li>
                         <li className="break" />
-                        {this.state.arrBreadcrumb.map((value, index) => {
-                            return (
-                                <span key={index}>
-                                    <li className="is-active">
-                                        <Link to="#">
-                                            <span itemProp="name">{value.category}</span>
-                                        </Link>
-                                        <meta itemProp="position" content={1} />
-                                    </li>
-                                    <li className="break" />
-                                </span>
-
-                            )
-                        })}
-
+                        <li className="is-active">
+                            <a itemProp="item">
+                                <span itemProp="name">Electronics</span>
+                            </a>
+                            <meta itemProp="position" content={1} />
+                        </li>
+                        <li className="break" />
                     </ul>
                     <form className="search-form" id="search-cate">
                         <input type="search" id="keyword-input-index" defaultValue="" className="keyword-input keyword-txt" placeholder="Tìm kiếm trong danh mục..." />
@@ -58,9 +37,7 @@ class Breadcrumbs extends React.Component {
 }
 function mapStateToProps(state) {
     return {
-        loadAdd: state.loadAdd,
-        mouseClickCategory: state.mouseClickCategory,
-        initLoadCategoryItem: state.initLoadCategoryItem,
+        loadAdd: state.loadAdd
     }
 }
 const connectedHomePage = connect(mapStateToProps)(Breadcrumbs);
