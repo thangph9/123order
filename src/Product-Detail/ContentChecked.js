997d7e7c214629bbc03d24bad3dc5480a70543d5
trjvjp97 <trjvjp97@gmail.com>
2018-10-18 09:37:38 +0700

    123

--- src/Product-Detail/ContentChecked.js
+++ src/Product-Detail/ContentChecked.js
@@ -35,13 +35,68 @@ class ContentChecked extends React.Component {
                     </div>{/* .block-head*/}
                     <div className="block-main">
                         <Slider {...settings}>
-                            <img className="pd-img img-lazy" src="https://images-eu.ssl-images-amazon.com/images/I/41%2B-Du4Ow7L._SL500_SY135_.jpg" alt="img" style={{}} />
+                            <div className="panel-product-scroll">
+                                <div className="img-pane">
+                                    <a className="img-field" href="/de/tablet-stander-gritin-handyhalterung-phone-dock-multi-winkel-aluminium-verstellbar-stabil-stand-fur-ipad-air-mini-2-3-4-ipad-pro-97-105-iphone-x-8-7-7-plus-6s-6-plus-kindle-nexus-tab-tisch-halterung-und-anderer-tablets-B07C3C55SV.html">
+                                        <span className="field-inner parent-img-loaded">
+
+                                        </span>
+                                    </a>
+                                </div>{/* .img-pane */}
+                                <a className="pd-title" href="/de/tablet-stander-gritin-handyhalterung-phone-dock-multi-winkel-aluminium-verstellbar-stabil-stand-fur-ipad-air-mini-2-3-4-ipad-pro-97-105-iphone-x-8-7-7-plus-6s-6-plus-kindle-nexus-tab-tisch-halterung-und-anderer-tablets-B07C3C55SV.html"><span className="title-inner" style={{ transitionDuration: '0s', marginLeft: 0 }}>Tablet ständer, Gritin Handyhalterung Phone Dock Multi Winkel Aluminium verstellbar Stabil Stand für iPad Air Mini 2 3 4, iPad Pro 9.7 / 10.5, iPhone X, 8, 7, 7 Plus 6s 6 / Plus, Kindle, Nexus, Tab, Tisch Halterung und anderer Tablets</span></a>
+                            </div>{/* .panel-product-box */}
+                            <div className="panel-product-scroll">
+                                <div className="img-pane">
+                                    <a className="img-field" href="/de/tablet-stander-gritin-handyhalterung-phone-dock-multi-winkel-aluminium-verstellbar-stabil-stand-fur-ipad-air-mini-2-3-4-ipad-pro-97-105-iphone-x-8-7-7-plus-6s-6-plus-kindle-nexus-tab-tisch-halterung-und-anderer-tablets-B07C3C55SV.html">
+                                        <span className="field-inner parent-img-loaded">
+
+                                        </span>
+                                    </a>
+                                </div>{/* .img-pane */}
+                                <a className="pd-title" href="/de/tablet-stander-gritin-handyhalterung-phone-dock-multi-winkel-aluminium-verstellbar-stabil-stand-fur-ipad-air-mini-2-3-4-ipad-pro-97-105-iphone-x-8-7-7-plus-6s-6-plus-kindle-nexus-tab-tisch-halterung-und-anderer-tablets-B07C3C55SV.html"><span className="title-inner" style={{ transitionDuration: '0s', marginLeft: 0 }}>Tablet ständer, Gritin Handyhalterung Phone Dock Multi Winkel Aluminium verstellbar Stabil Stand für iPad Air Mini 2 3 4, iPad Pro 9.7 / 10.5, iPhone X, 8, 7, 7 Plus 6s 6 / Plus, Kindle, Nexus, Tab, Tisch Halterung und anderer Tablets</span></a>
+                            </div>{/* .panel-product-box */}
+                            <div className="panel-product-scroll">
+                                <div className="img-pane">
+                                    <a className="img-field" href="/de/tablet-stander-gritin-handyhalterung-phone-dock-multi-winkel-aluminium-verstellbar-stabil-stand-fur-ipad-air-mini-2-3-4-ipad-pro-97-105-iphone-x-8-7-7-plus-6s-6-plus-kindle-nexus-tab-tisch-halterung-und-anderer-tablets-B07C3C55SV.html">
+                                        <span className="field-inner parent-img-loaded">
+
+                                        </span>
+                                    </a>
+                                </div>{/* .img-pane */}
+                                <a className="pd-title" href="/de/tablet-stander-gritin-handyhalterung-phone-dock-multi-winkel-aluminium-verstellbar-stabil-stand-fur-ipad-air-mini-2-3-4-ipad-pro-97-105-iphone-x-8-7-7-plus-6s-6-plus-kindle-nexus-tab-tisch-halterung-und-anderer-tablets-B07C3C55SV.html"><span className="title-inner" style={{ transitionDuration: '0s', marginLeft: 0 }}>Tablet ständer, Gritin Handyhalterung Phone Dock Multi Winkel Aluminium verstellbar Stabil Stand für iPad Air Mini 2 3 4, iPad Pro 9.7 / 10.5, iPhone X, 8, 7, 7 Plus 6s 6 / Plus, Kindle, Nexus, Tab, Tisch Halterung und anderer Tablets</span></a>
+                            </div>{/* .panel-product-box */}
+                            <div className="panel-product-scroll">
+                                <div className="img-pane">
+                                    <a className="img-field" href="/de/tablet-stander-gritin-handyhalterung-phone-dock-multi-winkel-aluminium-verstellbar-stabil-stand-fur-ipad-air-mini-2-3-4-ipad-pro-97-105-iphone-x-8-7-7-plus-6s-6-plus-kindle-nexus-tab-tisch-halterung-und-anderer-tablets-B07C3C55SV.html">
+                                        <span className="field-inner parent-img-loaded">
+
+                                        </span>
+                                    </a>
+                                </div>{/* .img-pane */}
+                                <a className="pd-title" href="/de/tablet-stander-gritin-handyhalterung-phone-dock-multi-winkel-aluminium-verstellbar-stabil-stand-fur-ipad-air-mini-2-3-4-ipad-pro-97-105-iphone-x-8-7-7-plus-6s-6-plus-kindle-nexus-tab-tisch-halterung-und-anderer-tablets-B07C3C55SV.html"><span className="title-inner" style={{ transitionDuration: '0s', marginLeft: 0 }}>Tablet ständer, Gritin Handyhalterung Phone Dock Multi Winkel Aluminium verstellbar Stabil Stand für iPad Air Mini 2 3 4, iPad Pro 9.7 / 10.5, iPhone X, 8, 7, 7 Plus 6s 6 / Plus, Kindle, Nexus, Tab, Tisch Halterung und anderer Tablets</span></a>
+                            </div>{/* .panel-product-box */}
+                            <div className="panel-product-scroll">
+                                <div className="img-pane">
+                                    <a className="img-field" href="/de/tablet-stander-gritin-handyhalterung-phone-dock-multi-winkel-aluminium-verstellbar-stabil-stand-fur-ipad-air-mini-2-3-4-ipad-pro-97-105-iphone-x-8-7-7-plus-6s-6-plus-kindle-nexus-tab-tisch-halterung-und-anderer-tablets-B07C3C55SV.html">
+                                        <span className="field-inner parent-img-loaded">
+
+                                        </span>
+                                    </a>
+                                </div>{/* .img-pane */}
+                                <a className="pd-title" href="/de/tablet-stander-gritin-handyhalterung-phone-dock-multi-winkel-aluminium-verstellbar-stabil-stand-fur-ipad-air-mini-2-3-4-ipad-pro-97-105-iphone-x-8-7-7-plus-6s-6-plus-kindle-nexus-tab-tisch-halterung-und-anderer-tablets-B07C3C55SV.html"><span className="title-inner" style={{ transitionDuration: '0s', marginLeft: 0 }}>Tablet ständer, Gritin Handyhalterung Phone Dock Multi Winkel Aluminium verstellbar Stabil Stand für iPad Air Mini 2 3 4, iPad Pro 9.7 / 10.5, iPhone X, 8, 7, 7 Plus 6s 6 / Plus, Kindle, Nexus, Tab, Tisch Halterung und anderer Tablets</span></a>
+                            </div>{/* .panel-product-box */}
+                            <div className="panel-product-scroll">
+                                <div className="img-pane">
+                                    <a className="img-field" href="/de/tablet-stander-gritin-handyhalterung-phone-dock-multi-winkel-aluminium-verstellbar-stabil-stand-fur-ipad-air-mini-2-3-4-ipad-pro-97-105-iphone-x-8-7-7-plus-6s-6-plus-kindle-nexus-tab-tisch-halterung-und-anderer-tablets-B07C3C55SV.html">
+                                        <span className="field-inner parent-img-loaded">
+
+                                        </span>
+                                    </a>
+                                </div>{/* .img-pane */}
+                                <a className="pd-title" href="/de/tablet-stander-gritin-handyhalterung-phone-dock-multi-winkel-aluminium-verstellbar-stabil-stand-fur-ipad-air-mini-2-3-4-ipad-pro-97-105-iphone-x-8-7-7-plus-6s-6-plus-kindle-nexus-tab-tisch-halterung-und-anderer-tablets-B07C3C55SV.html"><span className="title-inner" style={{ transitionDuration: '0s', marginLeft: 0 }}>Tablet ständer, Gritin Handyhalterung Phone Dock Multi Winkel Aluminium verstellbar Stabil Stand für iPad Air Mini 2 3 4, iPad Pro 9.7 / 10.5, iPhone X, 8, 7, 7 Plus 6s 6 / Plus, Kindle, Nexus, Tab, Tisch Halterung und anderer Tablets</span></a>
+                            </div>{/* .panel-product-box */}
                         </Slider>
 
-                        <div className="control-nav">
-                            <div className="swiper-button-prev swiper-button-disabled"><div className="btn-inner" /></div>
-                            <div className="swiper-button-next"><div className="btn-inner" /></div>
-                        </div>
                     </div>
                 </div>{/* .container*/}
             </section>
