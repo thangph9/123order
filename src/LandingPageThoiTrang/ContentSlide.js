9880e3ef3fd0beb246ded42f07bd3f5d946cf246
trjvjp97 <trjvjp97@gmail.com>
2018-10-18 18:10:35 +0700

    123

--- src/LandingPageThoiTrang/ContentSlide.js
+++ src/LandingPageThoiTrang/ContentSlide.js
@@ -30,10 +30,10 @@ class ContentSlide extends React.Component{
         return (
             <div style={{marginTop:'21px',maringBot:'15px'}}>     
                         <Slider {...settings}>
-                            <img alt='landing-img' src="https://static-v3.weshop.com.vn/upload/cms/18/09/08/k/d/v/7/v/1.jpg" style={{width:'1140px'}} />
-                            <img alt='landing-img' src="https://static-v3.weshop.com.vn/upload/cms/18/09/08/m/c/f/e/o/1349x500.jpg" style={{width:'1140px'}} />
-                            <img alt='landing-img' src="https://static-v3.weshop.com.vn/upload/cms/18/09/08/k/d/v/7/v/1.jpg" style={{width:'1140px'}} />
-                            <img alt='landing-img' src="https://static-v3.weshop.com.vn/upload/cms/18/09/08/m/c/f/e/o/1349x500.jpg" style={{width:'1140px'}} />
+                            <img alt='landing-img' src="https://static-v3.weshop.com.vn/upload/cms/18/10/09/d/e/n/w/a/1349x500.jpg" style={{width:'1140px'}} />
+                            <img alt='landing-img' src="https://static-v3.weshop.com.vn/upload/cms/18/10/09/v/4/a/n/k/1349x500.jpg" style={{width:'1140px'}} />
+                            <img alt='landing-img' src="https://static-v3.weshop.com.vn/upload/cms/18/10/09/d/e/n/w/a/1349x500.jpg" style={{width:'1140px'}} />
+                            <img alt='landing-img' src="https://static-v3.weshop.com.vn/upload/cms/18/10/09/3/7/l/6/g/1349x500.jpg" style={{width:'1140px'}} />
                         </Slider>
             </div>
         )
