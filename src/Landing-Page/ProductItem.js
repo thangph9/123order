a4ca327848eda9a1c8a51eb2a5ed58e314d51f14
trjvjp97 <trjvjp97@gmail.com>
2018-10-22 00:23:20 +0700

    123

--- src/Landing-Page/ProductItem.js
+++ src/Landing-Page/ProductItem.js
@@ -21,6 +21,10 @@ class ProductItem extends React.Component {
         }
         var {initLoadCurrencyRaito}=this.props;
         console.log(initLoadCurrencyRaito);
+        if(initLoadCurrencyRaito.length>0){
+            var base_priceCurrency=Number(data.base_price)*initLoadCurrencyRaito[0].raito
+            var priceCurrency=Number(price)*initLoadCurrencyRaito[0].raito
+        }
         titleTrim=titleTrim.replace(/%/g , "");
         titleTrim=titleTrim.replace(/ /g , "-");
         var starNumber=Number(data.star)*20;
@@ -59,8 +63,8 @@ class ProductItem extends React.Component {
 							</div>
 						</div>  
                         <br />
-                        <strong className="card-text align-left deal-price mb-2" style={{width: '97px',fontSize: '12px',fontFamily:'Arial,Helvetica,sans-serif'}}>{data.price}</strong>
-                        <span className="card-text align-right deal-old-price"><s style={{fontSize:'12px'}}>{data.base_price}</s></span>
+                        <strong className="card-text align-left deal-price mb-2" style={{width: '97px',fontSize: '12px',fontFamily:'Arial,Helvetica,sans-serif'}}>{priceCurrency}</strong>
+                        <span className="card-text align-right deal-old-price"><s style={{fontSize:'12px'}}>{base_priceCurrency}</s></span>
                         <div style={{ clear: 'both' }} />
                         <p className="card-text align-left deal-old-price"><i  className={(data.death_clock==='')? '' :'far fa-clock card-text'} /> {(data.death_clock==='None') ? '' : data.death_clock }</p>
                     </div>
