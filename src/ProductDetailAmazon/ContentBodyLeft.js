ef4bd871364e3c6c87808e6dd0d80c710742ab1d
trjvjp97 <trjvjp97@gmail.com>
2018-10-22 15:04:06 +0700

    123

--- src/ProductDetailAmazon/ContentBodyLeft.js
+++ src/ProductDetailAmazon/ContentBodyLeft.js
@@ -64,21 +64,20 @@ class ContentBodyLeft extends React.Component {
                             <span className="field-inner"><img className="over-img" src="https://static.fado.vn/f/desktop/v2/images/null-image.png" alt="img" /></span>
                         </div>
                         <div className="swiper-container swiper-container-horizontal" style={{overflow: 'visible'}}>
-                            <div className="swiper-wrapper" style={{ height: 310 }}>
+                            <div className="swiper-wrapper" style={{ height: 370 }}>
                                 <div className="swiper-slide" style={{ width: 310, marginRight: 10 }}>
                                     <a className="gallery-item img-item" data-fancybox="group" target="_blank">
                                         <div className="item-inner">
                                             <ReactImageMagnify className="xzoom" {...{
                                                 smallImage: {
                                                     alt: 'img',
-                                                    height:310,
+                                                    width: 310,
                                                     src: (loadDetail[0] != undefined) ? loadDetail[0].largeimage[mouseClick] : ''
 
                                                 },
                                                 largeImage: {
                                                     src: (loadDetail[0] != undefined) ? loadDetail[0].hugeimage[mouseClick] : '',
                                                     width: 1200,
-                                                    height: 1400
                                                 },
                                                 shouldUsePositiveSpaceLens: true,
                                                 enlargedImageContainerDimensions: {
