7054dda24b0a8c4755ce08245e261359d081ce31
trjvjp97 <trjvjp97@gmail.com>
2018-10-27 17:52:02 +0700

    123

--- src/ProductDetailTest/ProductDetailTest.js
+++ src/ProductDetailTest/ProductDetailTest.js
@@ -18,28 +18,7 @@ class ProductDetailTest extends React.Component {
         }
         this.handleScrollToElement = this.handleScrollToElement.bind(this); 
     }
-    componentWillMount(){
-        console.log(this.props.match.params.dealid);
-        axios.post('/product-detail',{
-            dealid:this.props.match.params.dealid
-        }).then(res=>{
-            var action=initLoadProductDetail(res.data[1].ProductDetail);
-            this.props.dispatch(mouseClickLinkProductItem(res.data[2]));
-            this.props.dispatch(mouseClickSmallImageProduct(0));
-            this.props.dispatch(action);
-        })
-        document.documentElement.scrollTop=0
-    }
-    componentDidMount() {
-        window.addEventListener('scroll', this.handleScrollToElement);
-    }
-    
-    componentWillUnmount() {
-        window.removeEventListener('scroll', this.handleScrollToElement);
-    }
-    handleScrollToElement() {
-        this.props.dispatch(mouseScrollDetailProduct(document.documentElement.scrollTop));
-    }
+   
     render() {
         return (
             <div className="body-product-detail"  ref="scrollProduct">
