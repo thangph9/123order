5da76dce4ed338d6f2993e01b9467a3f76e6e971
trjvjp97 <trjvjp97@gmail.com>
2018-10-27 15:17:09 +0700

    123

--- src/HomePage/ContentSaleLeft.js
+++ src/HomePage/ContentSaleLeft.js
@@ -44,7 +44,7 @@ class ContentSaleLeft extends React.Component {
 
         return (
             <div className="col-7 p-2">
-               <div ref="slick">
+               <div className="abc">
                    abc
                </div>
                 
