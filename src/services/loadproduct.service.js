5e6470aa0158169d4e43b8fa34f34fa80650208e
trjvjp97 <trjvjp97@gmail.com>
2018-10-27 07:06:07 +0700

    123

--- src/services/loadproduct.service.js
+++ src/services/loadproduct.service.js
@@ -9,7 +9,7 @@ const api={
 function getDealDay(add_item){
     const requestOptions={
         method: 'POST',
-        headers: {...authHeader(), 'Content-Type': 'application/json'},
+        headers: {'Content-Type': 'application/json'},
         body: JSON.stringify({addItem: add_item})
     }
     return fetch(api.getDealDay,requestOptions).then(handleResponse);
