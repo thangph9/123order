041bedca84072810324d48f070e39f3ac6faaa71
trjvjp97 <trjvjp97@gmail.com>
2018-10-28 20:50:04 +0700

    123

--- src/ProductDetailTest/ContentBodyDetailLeftTop.js
+++ src/ProductDetailTest/ContentBodyDetailLeftTop.js
@@ -9,7 +9,7 @@ class ContentBodyDetailLeftTop extends React.Component {
   render() {
     var { loadDetail } = this.props;
     var { mouseClick } = this.props;
-    console.log(loadDetail);
+    (loadDetai!=undefined&&loadDetail.length>0) &&console.log(loadDetail);
     return (
       <div className="big-image-product">
         <ReactImageMagnify style={{ zIndex: 3 }} className="xzoom" {...{
