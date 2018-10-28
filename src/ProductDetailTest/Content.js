94d37741496ec675de2bc5a2655127ffa40056ee
trjvjp97 <trjvjp97@gmail.com>
2018-10-28 14:51:53 +0700

    123

--- src/ProductDetailTest/Content.js
+++ src/ProductDetailTest/Content.js
@@ -1,7 +1,5 @@
 import React from "react";
 import { connect } from "react-redux";
-import './usexpressreal2.css'
-import './usexpressreal3.css'
 import {ContentBreadcrumb} from './ContentBreadcrumb'
 import { ContentBody } from "./ContentBody";
 
