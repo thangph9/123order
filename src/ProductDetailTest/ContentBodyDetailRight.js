b3b0ad728964f0f7d933b9afa5bed6f33abb93d4
trjvjp97 <trjvjp97@gmail.com>
2018-10-29 11:50:38 +0700

    123

--- src/ProductDetailTest/ContentBodyDetailRight.js
+++ src/ProductDetailTest/ContentBodyDetailRight.js
@@ -5,19 +5,19 @@ class ContentBodyDetailRight extends React.Component {
   constructor(props) {
     super(props);
   }
-  
+
   render() {
 
-    var {loadDetail}=this.props;
-    var {initLoadCurrencyRaito}=this.props;
+    var { loadDetail } = this.props;
+    var { initLoadCurrencyRaito } = this.props;
     console.log(initLoadCurrencyRaito);
-    if(loadDetail!=undefined){
-      var deal=loadDetail[1][0].deal_of_day*loadDetail[2][0].raito
-      var save=loadDetail[1][0].save_price*loadDetail[2][0].raito
-      var price=loadDetail[1][0].price*loadDetail[2][0].raito
+    if (loadDetail != undefined) {
+      var deal = loadDetail[1][0].deal_of_day * loadDetail[2][0].raito
+      var save = loadDetail[1][0].save_price * loadDetail[2][0].raito
+      var price = loadDetail[1][0].price * loadDetail[2][0].raito
     }
-    var starNumber=0;
-        if(loadDetail!=undefined) starNumber=Number(loadDetail[1][0].star)*20;
+    var starNumber = 0;
+    if (loadDetail != undefined) starNumber = Number(loadDetail[1][0].star) * 20;
     return (
       <div className="content col-md-8 col-sm-7 col-xs-12 col-tn-12">
         <div className="tt_sp_chitiet">
@@ -45,29 +45,41 @@ class ContentBodyDetailRight extends React.Component {
                   <span> Chưa có  đánh giá</span>
                 </div>
               </div>
+              <div class="tt_nguoiban">
+                Người bán: <strong>Ubio Labs</strong>
+              </div>
             </div>
             <div className="block_ttsp">
               <ul>
                 <li>
-                  <label>Giá ban đầu:</label>
+                  <label>Giá ban đầu của sản phẩm:</label>
                   <span className="tong_gia_vnd">
-                    {(loadDetail!=undefined)?Math.round(price).toLocaleString('en-US'):0} VNĐ
+                    {(loadDetail != undefined) ? Math.round(price).toLocaleString('en-US') : 0} VNĐ
                     </span>
                 </li>
                 <li>
-                  <label>Bạn tiết kiệm:</label>
+                  <label>Bạn tiết kiệm được:</label>
                   <span className="tong_gia_vnd">
-                  {(loadDetail!=undefined)?Math.round(save).toLocaleString('en-US'):0} VNĐ
-                  </span>      
+                    {(loadDetail != undefined) ? Math.round(save).toLocaleString('en-US') : 0} VNĐ
+                  </span>
                 </li>
                 <li>
-                  <label>Giá đã giảm: </label>
-                  <span className="tong_gia_vnd"> {(loadDetail!=undefined)?Math.round(deal).toLocaleString('en-US'):0} VNĐ</span>
+                  <label>Giá đã giảm của sản phẩm: </label>
+                  <span className="tong_gia_vnd"> {(loadDetail != undefined) ? Math.round(deal).toLocaleString('en-US') : 0} VNĐ</span>
                 </li>
               </ul>
               <span>Giá trên là giá cuối cùng, bạn không cần trả thêm bất kỳ khoản phí nào khác</span>
             </div>
             <div className="block_tk_tinhtrangbansp">
+              <div className="tinhtrang_sp">
+                <span className="time_today w_49">Hôm nay:</span>
+                <div className="sl_con pull-left w_49">Số lượng khuyến mại còn lại (17)</div>
+                <div className="sl_daban pull-right w_49">Đã bán(13)</div>
+              </div>
+              <div className="progress tientrinh_bansp w_49">
+                <div className="progress-bar" role="progressbar" aria-valuenow="56.6666666666667" aria-valuemin={0} aria-valuemax={100} style={{ width: '56.6666666666667%' }}>
+                </div>
+              </div>
               <div className="tieuchi_banhang">
                 <ul>
                   <li>Chi phí minh bạch</li>
@@ -104,31 +116,31 @@ class ContentBodyDetailRight extends React.Component {
             <div className="block_ttthanhtoan_chitiet" style={{ width: 241 }}>
               <ul>
                 <li>
-                  <h6>CHẤP NHẬN THANH TOÁN</h6>
+                  <h6 style={{ fontSize: '12px', fontWeight: 600 }}>CHẤP NHẬN THANH TOÁN</h6>
                   <div>
                     <label >Thẻ thanh toán nội địa</label>
-                    <span><img width="18" height="18" src="https://usexpress.vn/Media_Master/images/thett_1.svg" alt="img"/></span>
+                    <span><img width="18" height="18" src="https://usexpress.vn/Media_Master/images/thett_1.svg" alt="img" /></span>
                   </div>
                   <div className="the_thtoan">
                     <label >Thẻ Visa/Master/JCB</label>
-                    <span><img width={18} height={18} src="https://usexpress.vn/Media_Master/images/thett_2.svg" alt="img"/></span>
-                    <span><img width={18} height={18} src="https://usexpress.vn/Media_Master/images/thett_3.svg" alt="img"/></span>
-                    <span><img width={18} height={18} src="https://usexpress.vn/Media_Master/images/thett_4.svg" alt="img"/></span>
+                    <span><img width={18} height={18} src="https://usexpress.vn/Media_Master/images/thett_2.svg" alt="img" /></span>
+                    <span><img width={18} height={18} src="https://usexpress.vn/Media_Master/images/thett_3.svg" alt="img" /></span>
+                    <span><img width={18} height={18} src="https://usexpress.vn/Media_Master/images/thett_4.svg" alt="img" /></span>
                   </div>
                 </li>
                 <li>
-                  <h6>HỆ THỐNG TỰ ĐỘNG</h6>
+                  <h6 style={{ fontSize: '12px', fontWeight: 600 }}>HỆ THỐNG TỰ ĐỘNG</h6>
                   <div>
                     <label >Xử lý đơn hàng với 123order</label>
-                    <span><img width={18} height={13} src="https://usexpress.vn/Media_Master/images/muahang_1.svg" alt="img"/></span>
+                    <span><img width={18} height={13} src="https://usexpress.vn/Media_Master/images/muahang_1.svg" alt="img" /></span>
                   </div>
                   <div>
                     <label >Thông báo tình trạng đơn hàng</label>
-                    <span><img width={18} height={18} src="https://usexpress.vn/Media_Master/images/muahang_2.svg" alt="img"/></span>
+                    <span><img width={18} height={18} src="https://usexpress.vn/Media_Master/images/muahang_2.svg" alt="img" /></span>
                   </div>
                 </li>
                 <li>
-                  <h6>MIỄN PHÍ VẬN CHUYỂN TOÀN QUỐC</h6>
+                  <h6 style={{ fontSize: '12px', fontWeight: 600 }}>MIỄN PHÍ VẬN CHUYỂN TOÀN QUỐC</h6>
                   <div>
                     <label >Nhận hàng trong vòng 2 tuần nếu thanh toán ngay hôm nay</label>
                     <span><img width={19} height={19} src="https://usexpress.vn/Media_Master/images/muahang_3.svg" alt="img" /></span>
