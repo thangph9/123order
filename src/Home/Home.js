fcbf2c33ab0dcf86d21e6e8b5c93ad758670348c
trjvjp97 <trjvjp97@gmail.com>
2018-11-02 05:35:15 +0700

    123

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -21,7 +21,7 @@ class Home extends React.Component {
       <div className="header__header___1t3MH false">
         <nav className="header__my-navbar___2Cghd header__navbar-toggleable-sm___pR4tF header__nav-header___3lWCb">
           <div className="container__container___1fvX0 header__container___2d-Oi">
-            <div className="hidden-md-up header__my-navbar-toggler___2PiaS"><button className="header__btn-icon___17D-i" type="button"><i className="ic-ic-user" /></button></div><a className="header__navbar-brand___SzzgD" href="/"><img src="img/logo123order.png" /></a>
+            <div className="hidden-md-up header__my-navbar-toggler___2PiaS"><button className="header__btn-icon___17D-i" type="button"><i className="ic-ic-user" /></button></div><a className="header__navbar-brand___SzzgD" href="/"><img style={{position:'relative',top:'4px'}} src="img/logo123order.png" /></a>
             <div className="clearfix collapse header__navbar-collapse___2AK1h"><a className="hidden-md-up header__navbar-brand___SzzgD" href="/"><img src="img/logo123order.png" /></a>
               <ul className="hidden-md-down header__navbar-nav___9cfBy header__navbar-left___25OFe">
                 <li className="header__nav-item___MQLXP"><a className="header__nav-link___3W4sc " href="/Women">Amazon</a></li>
